6405f5db2e61006bd70b0dd9b68184caed262dcd
Keith <laurence.albano18@gmail.com>
2020-05-29 03:26:26 +0800

    Updates

---
+++ precache-manifest.3d819419c31a5b1b004a5e4d4d015432.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e171edc53dce3a61215a819f26748bd3",
+    "url": "/wehost-solutions/index.html"
+  },
+  {
+    "revision": "c3f5ddf3eede62918608",
+    "url": "/wehost-solutions/static/css/main.ed3a0baa.chunk.css"
+  },
+  {
+    "revision": "8ec60d9ac6724fe1a580",
+    "url": "/wehost-solutions/static/js/2.db86fcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wehost-solutions/static/js/2.db86fcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f5ddf3eede62918608",
+    "url": "/wehost-solutions/static/js/main.fc493f43.chunk.js"
+  },
+  {
+    "revision": "12f807c32f55727230b0",
+    "url": "/wehost-solutions/static/js/runtime-main.a0d54178.js"
+  },
+  {
+    "revision": "ceb026de2c52677a08ce9221cb7537e0",
+    "url": "/wehost-solutions/static/media/dev1.ceb026de.png"
+  },
+  {
+    "revision": "5c6a8722bc16ca7e2d294b3ab8bcb2f2",
+    "url": "/wehost-solutions/static/media/dev2.5c6a8722.png"
+  },
+  {
+    "revision": "36e013b0800b82b8d26745abc4ca42ce",
+    "url": "/wehost-solutions/static/media/dev3.36e013b0.png"
+  },
+  {
+    "revision": "e90d311a53f1a8feb48a286314266d75",
+    "url": "/wehost-solutions/static/media/dev4.e90d311a.png"
+  },
+  {
+    "revision": "7c2f49b4f791c9c0d6bbe0c5eb418e8f",
+    "url": "/wehost-solutions/static/media/logo.7c2f49b4.png"
+  },
+  {
+    "revision": "86cdb59732ddcb6d7a94ef96f8822eba",
+    "url": "/wehost-solutions/static/media/parallaxbg.86cdb597.jpg"
+  },
+  {
+    "revision": "3e182a4d95ebce01df6748ae7c072954",
+    "url": "/wehost-solutions/static/media/parallaxbtmbg.3e182a4d.jpg"
+  },
+  {
+    "revision": "cc652a35aaba4e31b068a30ec8d2eb8e",
+    "url": "/wehost-solutions/static/media/parallaxbtmbg3.cc652a35.jpg"
+  }
+]);
