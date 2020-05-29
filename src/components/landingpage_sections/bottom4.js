8012fee920ee87f2ac3ebbbeea37f8115e408d54
Keith <laurence.albano18@gmail.com>
2020-05-29 12:55:51 +0800

    added form functions

--- src/components/landingpage_sections/bottom4.js
+++ src/components/landingpage_sections/bottom4.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
 
 export class bottom4 extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             email: '',
+             notify: 'Notify me'
+        }
+    }
+
+    emailChangeHandler = (event) => {
+        this.setState({email: event.target.value});
+    }
+
+    onSubmit = (event) => {
+        alert(`${this.state.email}`);
+        event.preventDefault();
+    }
+    
     render() {
+        const {email, notify} = this.state;
         return (
             <div id="bottom4">
                 <div className="wrapper">
@@ -10,9 +29,9 @@ export class bottom4 extends Component {
                             <h4>Want news and updates? <span>Sign up for our newsletter to stay up to date. </span></h4>
                         </div>
                         <div className="btm4_box2">
-                            <form action="">
-                                <input type="email" placeholder="Email"/>
-                                <input type="submit" value="Notify me"/>
+                            <form onSubmit={this.onSubmit}>
+                                <input type="email" placeholder="Email" value={email} onChange={this.emailChangeHandler}/>
+                                <input type="submit" value={notify} onSubmit={this.onSubmit}/>
                             </form>
                             <p>We care about the protection of your data. </p>
                         </div>
--- src/components/landingpage_sections/bottom5.js
+++ src/components/landingpage_sections/bottom5.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
 
 export class bottom5 extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             email: '',
+             subject: '',
+             message: '',
+             buttonMessage: 'Send Message'
+        }
+    }
+
+    emailChangeHandler = (event) => {
+        this.setState({ email: event.target.value });
+    };
+    
+    subjectChangeHandler = (event) => {
+        this.setState({ subject: event.target.value });
+    };
+
+    messageChangeHandler = (event) => {
+        this.setState({ message: event.target.value });
+    };
+    
+    handleSubmit = (event) => {
+        alert(`${this.state.email} ${this.state.subject} ${this.state.message}`);
+        event.preventDefault();
+    }
+
     render() {
+        const {email, subject, message, buttonMessage} = this.state;
         return (
             <div id="bottom5">
                 <div className="wrapper">
@@ -10,12 +39,12 @@ export class bottom5 extends Component {
                         <h5>Send us a message and discuss your projects.</h5>
                         <div className="line_styler"></div>
 
-                        <form action="#">
-                            <input type="email" placeholder="Email" required/>
-                            <input type="text" placeholder="Subject" required/>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="email" placeholder="Email" value={email} onChange={this.emailChangeHandler} required/>
+                            <input type="text" placeholder="Subject" value={subject} onChange={this.subjectChangeHandler} required/>
                             <div className="clearfix"></div>
-                            <textarea placeholder="Message" required></textarea>
-                            <input type="submit" value="Send Message"/>
+                            <textarea placeholder="Message" value={message} onChange={this.messageChangeHandler} required></textarea>
+                            <input type="submit" value={buttonMessage} onSubmit={this.handleSubmit}/>
                         </form>
                     </div>
                 </div>
